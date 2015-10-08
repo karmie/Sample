bad93ae1bcc25245a9abd08e52ac6c75fe4850dd
Karmie <sheep1014@gmail.com>
2015-10-08 14:07:02 +0800

    use Binding to Show option of Listbox

---
+++ Sample/ViewModels/ViewModel3.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample
+{
+    class ViewModel3
+    {
+        public ObservableCollection<Database> color { get; set; }
+
+        public ViewModel3()
+        {
+            color = new ObservableCollection<Database>
+            {
+                new Database { Color="Red" },
+                new Database { Color="Blue" },
+                new Database { Color="Yellow" },
+                new Database { Color="Black" },
+            };
+
+        }
+
+
+    }
+}
