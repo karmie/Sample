a6b0536599512ad4e2abd7599b3ecb9085de2df9
Karmie <sheep1014@gmail.com>
2015-10-08 15:27:03 +0800

    Add function the color of text "List" will be change.

--- Sample/ViewModels/ViewModel3.cs
+++ Sample/ViewModels/ViewModel3.cs
@@ -4,12 +4,15 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace Sample
 {
-    class ViewModel3
+    class ViewModel3 : Update
     {
         public ObservableCollection<Database> color { get; set; }
+
+        public ICommand changeColor { get; set; }
 
         public ViewModel3()
         {
@@ -21,6 +24,58 @@ namespace Sample
                 new Database { Color="Black" },
             };
 
+            FontColor = "Blue";
+
+            changeColor = new RelayCommand(changecolor, CanExecute);
+
+        }
+
+        string _FontColor;
+
+        public string FontColor
+        {
+            get
+            {
+                return _FontColor;
+            }
+            set
+            {
+                if (_FontColor != value)
+                {
+                    _FontColor = value;
+
+
+                }
+            }
+        }
+
+
+        public ICommand ChangeColor
+        {
+            get
+            {
+                return changeColor;
+            }
+            set
+            {
+                changeColor = value;
+            }
+        }
+
+        public void changecolor(object SelectedItem)
+        {
+
+            var C = SelectedItem as Database;
+            FontColor = C.Color;
+
+            RaisePropertyChanged("FontColor");
+
+
+        }
+
+        bool CanExecute(object parameter)
+        {
+            return true;
         }
 
 
